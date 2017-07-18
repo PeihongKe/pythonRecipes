2c9732f1fd225590e03f8ced03119645e545822c
PeihongKe <pkukph@hotmail.com>
2017-07-18 21:23:03 +0100

    add frozen set and add tuple as dict key

--- collection/builtinColl.py
+++ collection/builtinColl.py
@@ -1,5 +1,6 @@
 import util
 
+
 class TestList(util.TestCaseBase):
     """ test builtin List"""
 
@@ -9,3 +10,20 @@ class TestList(util.TestCaseBase):
         b = ['b']
         self.assertEqual(b.extend(a), None)
         self.assertEqual(b, ['b', 'a'])
+
+
+class TestFrozenSet(util.TestCaseBase):
+    def test_frozen_set(self):
+        f = frozenset({1, 2})
+        print(f)
+
+
+class TestDict(util.TestCaseBase):
+    """ """
+
+    def test_tuple_key(self):
+        """ """
+        d = dict()
+        d[1, 2] = 3
+        self.assertTrue([k for k in d.keys()][0], (1, 2))
+
