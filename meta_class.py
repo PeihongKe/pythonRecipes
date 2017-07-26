124f13ff8bcba4bc16f3c7484ea18e67680f81e9
PeihongKe <pkukph@hotmail.com>
2017-07-26 23:01:12 +0100

    add more meta class

--- meta_class.py
+++ meta_class.py
@@ -58,6 +58,7 @@ class TestMetaClassBasic(util.TestCaseBase):
 
 class InterfaceMeta(type):
     ''' a metaclass inherit from the default meta class type'''
+
     def __new__(cls, name, parents, dct):
         # create a class_id if it's not specified
         if 'class_id' not in dct:
@@ -71,15 +72,17 @@ class InterfaceMeta(type):
         # we need to call type.__new__ to complete the initialization
         return super(InterfaceMeta, cls).__new__(cls, name, parents, dct)
 
+
 class TestMetaClassModifyingAttributes(util.TestCaseBase):
     """ test custom metaclass modifying attributes
     example from: https://jakevdp.github.io/blog/2012/12/01/a-primer-on-python-metaclasses/
     """
+
     def test_interface_metaclass(self):
         """ test InterfaceMeta"""
         interface = InterfaceMeta('Interface', (), dict(file='tmp.txt'))
         self.assertTrue(type(interface) == InterfaceMeta)
-        self.assertFalse(type(interface) ==  type)
+        self.assertFalse(type(interface) == type)
         self.assertEqual(interface.class_id, 'interface')
         self.assertEqual(str(type(interface.file)), r"<class '_io.TextIOWrapper'>")
 
@@ -99,22 +102,78 @@ class DBInterfaceMeta(type):
 
         super(DBInterfaceMeta, cls).__init__(name, bases, dct)
 
+
 class DBInterface(metaclass=DBInterfaceMeta):
     """ """
 
 
 class TestMetaClassRegisterSubClasses(util.TestCaseBase):
     """ use case of meta class """
+
     def test_1(self):
         """ """
         self.assertEqual(DBInterface.registry, {})
 
     def test_2(self):
         """ """
+
         class FirstInterface(DBInterface):
             pass
 
         self.assertTrue('firstinterface' in DBInterface.registry.keys())
 
 
+MyMeta_New_Arg = {}
+MyMeta_Init_Arg = {}
 
+
+class MyMeta(type):
+    """  """
+
+    def __new__(meta, name, bases, dct):
+        """ """
+        MyMeta_New_Arg['meta'] = meta
+        MyMeta_New_Arg['name'] = name
+        MyMeta_New_Arg['bases'] = bases
+        MyMeta_New_Arg['dct'] = dct
+        return super(MyMeta, meta).__new__(meta, name, bases, dct)
+
+    def __init__(cls, name, bases, dct):
+        """ """
+        MyMeta_Init_Arg['cls'] = cls
+        MyMeta_Init_Arg['name'] = name
+        MyMeta_Init_Arg['bases'] = bases
+        MyMeta_Init_Arg['dct'] = dct
+        return super(MyMeta, cls).__init__(name, bases, dct)
+
+    #def __prepare__(metacls, name, bases):
+
+    #def __call__(self, *args, **kwargs):
+
+
+class MyKlass(object, metaclass=MyMeta):
+    def foo(self, param):
+        pass
+
+    barattr = 2
+
+
+class TestMetaClassMethods(util.TestCaseBase):
+    """ a meta class's method __new__ __init__ and __call"""
+
+    def test_meta_class_new_arg(self):
+        """ test the argument of meta class's __new__"""
+        self.assertEqual(MyMeta_New_Arg['meta'], MyMeta)
+        self.assertEqual(MyMeta_New_Arg['name'], 'MyKlass')
+        self.assertEqual(MyMeta_New_Arg['bases'], (object,))
+        self.assertTrue('foo' in MyMeta_New_Arg['dct'].keys())
+        self.assertTrue('barattr' in MyMeta_New_Arg['dct'].keys())
+
+    def test_meta_class_init_arg(self):
+        """ test the arguments of meta class's __init__ """
+        k = MyKlass()
+        self.assertEqual(MyMeta_Init_Arg['cls'], MyKlass)
+        self.assertEqual(MyMeta_Init_Arg['name'], 'MyKlass')
+        self.assertEqual(MyMeta_Init_Arg['bases'], (object,))
+        self.assertTrue('foo' in MyMeta_Init_Arg['dct'].keys())
+        self.assertTrue('barattr' in MyMeta_Init_Arg['dct'].keys())
