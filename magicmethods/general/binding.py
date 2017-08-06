10e8bb1f4f62f42ee6b03e4121320d6419b27a52
PeihongKe <pkukph@hotmail.com>
2017-08-07 00:05:22 +0100

    binding setattr

--- magicmethods/general/binding.py
+++ magicmethods/general/binding.py
@@ -1,1 +1,22 @@
 #http://minhhh.github.io/posts/a-guide-to-pythons-magic-methods
+import util
+
+class A(object):
+    """ a test class"""
+    # pass
+
+class TestBindingBasic(util.TestCaseBase):
+    """ """
+    def test_setattr(self):
+        """ (object, name, value)
+        setattr(x, 'foobar', 123) is equivalent to x.foobar = 123
+        """
+        a = A()
+        setattr(a, 'abc', 1)
+        a.efg = 2
+
+        self.assertEqual(a.abc, 1)
+        self.assertEqual(a.efg, 2)
+
+
+
