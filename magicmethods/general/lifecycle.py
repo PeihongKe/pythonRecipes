a93b845abbcfa63af23c35d54e91988f868cbf50
PeihongKe <pkukph@hotmail.com>
2017-08-07 00:05:42 +0100

    add __new__ example

--- magicmethods/general/lifecycle.py
+++ magicmethods/general/lifecycle.py
@@ -1,6 +1,7 @@
-import util
-
+import datetime
 from os.path import join
+
+import util
 
 
 class FileObject(object):
@@ -15,3 +16,23 @@ class FileObject(object):
         del self.file
 
 
+class A(object):
+    """ a class with __new__ defined """
+    def __new__(cls, *args, **kwargs):
+        """ __new__ is a static method, i.e., on class level """
+        new_instance = object.__new__(cls, *args, **kwargs)
+        setattr(new_instance, 'created_at', datetime.datetime.now())
+        return new_instance
+
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+
+
+class TestLifeCycleFunctions(util.TestCaseBase):
+    """ """
+
+    def test_new_type(self):
+        """ __new__ receives all the arguments that we pass while calling the class.
+        __new__(cls, *args, **kwargs)
+        """
+        print(type(object.__dict__['__new__']))
