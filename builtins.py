d13b44792677627977999600581e14f9f7f3d650
PeihongKe <pkukph@hotmail.com>
2018-07-13 23:17:42 +0100

    iterator

--- builtins.py
+++ builtins.py
@@ -1,5 +1,32 @@
 import util
 
+class PowTwo:
+    """class to implement an iterator protocol  """
+    def __init__(self, max = 0):
+        self.max = max
+
+    def __iter__(self):
+        """The __iter__() method returns the iterator object itself. If required, some initialization can be performed."""
+        self.n = 0
+        return self # needs to return the iterator object itself
+
+    def __next__(self):
+        """The __next__() method must return the next item in the sequence. On reaching the end, and in subsequent calls, it must raise StopIteration."""
+        if self.n<self.max  :
+            res = 2** self.n
+            self.n +=1
+            return result
+        else:
+            raise StopIteration # to indicate foo loop that it stops
+
+class TestIterator(util.TestCaseBase):
+
+    def test_container(self):
+        """get iterator from container"""
+        list = [1,2,3,4]
+        iterator = iter(list)
+        print(next(iterator))
+
 
 class TestBuildIn(util.TestCaseBase):
     ''' built in function '''
